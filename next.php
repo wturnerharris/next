4bd661f408316addd948ceef0fe93dbe2a25ab2e
Wesley Turner-Harris <wturnerharris@gmail.com>
2015-01-26 17:31:13 -0500

    Utilize backend sorting

--- next.php
+++ next.php
@@ -23,11 +23,20 @@ class Next_Train_API {
 	}
 	
 	// return random entry
-	private function get_stops() {
-		$sql = "SELECT * FROM stops WHERE location_type = 1;";
+	private function get_nearest_stops() {
+		$sql = "SELECT stop_id, stop_lat, stop_lon FROM stops WHERE location_type = 1;";
 		$query = $this->mysqli->query($sql);
-		$result = $query->num_rows ? $query->fetch_all(MYSQLI_ASSOC) : false;
-		return $result;
+		$result = $query->num_rows ? $query->fetch_all(MYSQLI_ASSOC) : array();
+		$locations = array();
+
+		foreach( $result as $train ) {
+			$locations[$train['stop_id']] = array(
+				'lat' => $train['stop_lat'],
+				'lon' => $train['stop_lon']
+			);
+		}
+
+		return $this->get_nearest_trains($locations);
 	}
 
 	private function get_times_by_stop_id($train_id, $limit = 5, $cardinality = '%') {
@@ -39,6 +48,49 @@ class Next_Train_API {
 		$query = $this->mysqli->query($sql);
 		$result = $query->num_rows ? $query->fetch_all(MYSQLI_ASSOC) : false;
 		return $result;
+	}
+
+	public function get_nearest_trains( $locations ){
+		$lat = (float)@$_GET['lat'];
+		$lon = (float)@$_GET['lon'];
+		$threshold = 1; // in miles
+		$distances = $return = array();
+		foreach( $locations as $stop_id => $stop ) {
+			$distance = $this->get_distance($lat, $lon, (float)$stop['lat'], (float)$stop['lon']);
+			if ( $distance <= $threshold ) $distances[$stop_id] = $distance;
+		}
+
+		// compare the distances in the array
+		asort($distances);
+		reset($distances);
+		
+		foreach ( $distances as $stop_id => $distance) {
+			array_push( $return, array( $stop_id => $distance ));
+		}
+		return $return;
+	}
+
+	/**
+	 * This routine calculates the distance between two points (given the
+	 * latitude/longitude of those points). It is being used to calculate
+	 * the distance between two locations using GeoDataSource(TM) Products
+	 *
+	 * @param   $args array latitude/longitude coordinates for two locations
+	 * @return  float distance
+	 */
+	public function get_distance($lat1, $lon1, $lat2, $lon2, $unit = "M") {
+		$theta = $lon1 - $lon2;
+		$dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2));
+		$dist += (cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta)));
+		$dist = acos($dist);
+		$dist = rad2deg($dist);
+		$miles = $dist * 60 * 1.1515;
+		
+		switch( strtoupper($unit) ) {
+			case 'K' : return ($miles * 1.609344);
+			case 'N': return ($miles * 0.8684);
+			case 'M': return $miles;
+		}
 	}
 
 	public function is_valid_request($action) {
@@ -63,11 +115,13 @@ class Next_Train_API {
 				$limit = @$_GET['limit'];
 				$direction = @$_GET['direction'];
 				
-				$times = get_times_by_stop_id();
+				$times = get_times_by_stop_id($train_id);
 			break;
-			default:
-				$stops = $this->get_stops();
-				$json = array( 'status' => ($stops?200:400), 'payload' => $stops); 
+			case 'getTrains':
+				$json = array( 
+					'status' => ($stops?200:400), 
+					'payload' => $this->get_nearest_stops(),
+				); 
 			break;
 		}
 		return $json;
---
+++ pseudo.md
@@ -0,0 +1,13 @@
+## Pseudocode
+
+#### Application Lifecycle
+
+1. Load App (startup screen)
+2. Load Main Activity View
+3. Load Dedicated Line View
+3. Toggle Cardinality
+
+#### Application Code
+1. Get 10 nearest stations
+2. Get next available times per station for a single direction
+3. Get next times per train
